d3ef8bc5bd9af30b9dcbcee090b143fb1a9b9b07
vinaykavuri <kavurivinay@gmail.com>
2020-02-28 14:08:15 -0500

    Rejas Raj is yes

--- app/src/main/java/com/kavurivinay/lab05/MainActivity.java
+++ app/src/main/java/com/kavurivinay/lab05/MainActivity.java
@@ -2,42 +2,76 @@ package com.kavurivinay.lab05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    VariableCounter counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = new VariableCounter();
+        sharedPreferences= getSharedPreferences("clickValues", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        counter.setCreate(counter.getCreate() + 1);
+        editor.putInt("create", sharedPreferences.getInt("create", 0) + 1);
+        editor.apply();
     }
 
     public void onResume(View view) {
+        editor.putInt("resume", sharedPreferences.getInt("resume", 0) + 1);
+        editor.apply();
+        counter.setResume(counter.getResume() + 1);
     }
 
     public void onStop(View view) {
+        editor.putInt("stop", sharedPreferences.getInt("stop", 0) + 1);
+        editor.apply();
+        counter.setStop(counter.getStop() + 1);
 
     }
 
     public void onStart(View view) {
-
+        editor.putInt("start", sharedPreferences.getInt("start", 0) + 1);
+        editor.apply();
+        counter.setStart(counter.getStart() + 1);
     }
 
     public void onRestart(View view){
-
+        editor.putInt("restart", sharedPreferences.getInt("restart", 0) + 1);
+        editor.apply();
+        counter.setRestart(counter.getRestart() + 1);
     }
 
     public void onDestroy(View view) {
+        editor.putInt("destroy", sharedPreferences.getInt("destroy", 0) + 1);
+        editor.apply();
+        counter.setDestroy(counter.getDestroy() + 1);
 
     }
 
     public void onPause(View view) {
+        editor.putInt("pause", sharedPreferences.getInt("pause", 0) + 1);
+        editor.apply();
+        counter.setPause(counter.getPause() + 1);
     }
 
     private class VariableCounter{
         int create, resume, stop, start, restart, destroy, pause;
-        public VariableCounter(){}
+        public VariableCounter(){
+            create = 0;
+            resume = 0;
+            stop = 0;
+            start = 0;
+            restart = 0;
+            destroy = 0;
+            pause = 0;
+        }
         public int getCreate() {
             return create;
         }
